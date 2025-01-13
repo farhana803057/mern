55ba4171b41925dc96b26bb31c1f77c750800deb
farhana803057 <farhanaibrahimn@gmail.com>
2025-01-13 16:08:47 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "fanah",
+    age: 20,
+    occupation:"student"
+}
+console.log(person.name)
+let people = [
+    { name: "fanah", age: 20, occupation: student },
+    { name: "sruthi", age: 30, occupation: teacher },
+    { name: "liyah", age: 40, occupation: begger},
+
+]
+console.log[people[1]]
